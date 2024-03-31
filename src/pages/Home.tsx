6900be2f57c406eb2b329c0c3851169fd5e2dd8b
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2024-03-31 14:14:55 +0200

    section searchok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import Nav from "../components/Nav";
+import Search from "../components/Search";
 
 export default function Home() {
-  return (<Nav />);
+  return (
+    <>
+      <Nav />
+      <Search />
+    </>
+  );
 }
