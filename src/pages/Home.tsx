2cb152e9b40db9d75f4321f63cdc36d2e7663eae
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2024-04-07 16:07:01 +0200

    set horizontal navBar with translate X animation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Nav from "../components/Nav";
 import Search from "../components/Search";
+import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <>
       <Nav />
       <Search />
+      <Categories />
     </>
   );
 }
